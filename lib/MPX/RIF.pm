6fe5ca161ec60468526ccadc67c48f504e9cfb45
mokko <mauricemengel@gmail.com>
2011-11-07 12:00:09 -0500

    mpx-rif now creates multimediaobjekt/@typ if defined in config/ex.yml

--- lib/MPX/RIF.pm
+++ lib/MPX/RIF.pm
@@ -628,6 +628,7 @@ sub writeXML {
 		my $now  = ConvertDate( ParseDateString("epoch $time") );
 
 		#new mulId now with number dependent file suffix
+		my $typ = $self->{data}->{$id}->get('typ');
 		my $objId  = $self->{data}->{$id}->get('verknüpftesObjekt');
 		my $pref   = $self->{data}->{$id}->get('pref');
 		my $suffix = $self->{data}->{$id}->get('multimediaErweiterung');
@@ -651,6 +652,12 @@ sub writeXML {
 				'mulId'       => $mulId,
 				'quelle'      => 'mpx-rif',
 			);
+
+			#Typ is optional
+			if ($typ) {
+				$attributes{typ}=$typ
+			}
+
 
 			#my $pref=$resource->get('pref');
 			if ( my $pref = $resource->get('pref') ) {
